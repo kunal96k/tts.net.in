1e9d5a6029408a9549792cdee7766dca0a74a432
unknown <kunalpatil192001@gmail.com>
2026-07-24 14:10:04 +0530

    Fix: Extract and apply page-specific styles from all original HTML templates to React pages

--- tts-react/src/main.jsx
+++ tts-react/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/style.css'
 import './css/premium.css'
+import './css/pages.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
